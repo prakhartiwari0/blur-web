60cd692e5b651be17a901c2e4beec734640d6820
Prakhar Tiwari <65062036+prakhartiwari0@users.noreply.github.com>
2023-10-16 22:40:46 +0530

    Add first files

---
+++ src/scripts/blur.js
@@ -0,0 +1,11 @@
+
+import './blur.css'
+
+
+let blurDiv = document.createElement('div')
+
+blurDiv.classList.add('blurDiv')
+
+// blurDiv.innerText = "..........................................................................................."
+
+document.body.appendChild(blurDiv)
